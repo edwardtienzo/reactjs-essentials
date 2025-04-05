09578b40a9a0a1dd6d129e1153a86786cb94aa25
Edward Tienzo <edward.tienzo@gmail.com>
2025-04-05 18:13:04 +0800

    Rearranged the files

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import './css/main.css'
+import App from './components/App.jsx'
+import Counter from './components/Counter.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
+    <Counter />
   </StrictMode>,
 )
